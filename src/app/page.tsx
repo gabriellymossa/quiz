6cf54c2d819bf78349babceb50ae093e7a2a2e41
gabriellymossa <aggs2003@gmail.com>
2026-01-05 20:30:26 -0300

    interface pronta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 "use client"
 
+import { useState } from "react";
+import { questions } from "./data/questions";
+
 const Page = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const title = 'Quiz de Culinária';
+
+
   return(
-    <div className=""></div>
+    <div className="w-full h-screen flex justify-center items-center bg-blue-950">
+      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
+        <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
+
+        <div className="p-5"> 
+          perguntas 
+        </div>
+
+        <div className="p-5 text-center border-t border-gray-300"> 
+          x de {questions.length} pergunta{questions.length === 1? '' : 's'}
+        </div>
+
+      </div>
+    </div>
   );
 }
 

7dd57ba32bf54e4101c6c77b98131a205de51024
gabriellymossa <aggs2003@gmail.com>
2026-01-05 21:22:32 -0300

    results criado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,26 @@
 import { useState } from "react";
 import { questions } from "./data/questions";
 import { QuestionItem } from "./components/QuestionItem";
+import { Results } from "./components/Results";
 
 const Page = () => {
+  const [answers, setAnswers] = useState<number[]>([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
   const title = 'Quiz de Culinária';
 
-  const handleAnswered = (answer: number) => {
-
+  const loadNextQuestion = () => {
+    if(questions[currentQuestion + 1]){
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResult(true);
+    }
   }
 
+  const handleAnswered = (answer: number) => { 
+    setAnswers([ ...answers, answer ]);
+    loadNextQuestion();
+   }
 
   return(
     <div className="w-full h-screen flex justify-center items-center bg-blue-950">
@@ -19,15 +30,24 @@ const Page = () => {
         <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
 
         <div className="p-5"> 
+          {!showResult &&
           <QuestionItem 
             question={questions[currentQuestion]}
             count={currentQuestion + 1}
             onAnswer={handleAnswered}
-          /> 
+          />}
+          {showResult && 
+            <Results questions={questions} answer={answers}/>
+          }
         </div>
 
         <div className="p-5 text-center border-t border-gray-300"> 
-          {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1? '' : 's'}
+          {!showResult && 
+          `${currentQuestion + 1} de ${questions.length} pergunta${questions.length === 1? '' : 's'}`
+          }
+          {showResult &&
+            <button className="px-3 py-2 rounded-md text-white bg-blue-800">Reiniciar Quiz</button>
+          }
         </div>
 
       </div>
